a78d7e6b41c79ad13b1960c9857bbd5623e7a304
Denys <denya@MacBook-Air-Anastasia.local>
2023-10-01 18:27:47 +0200

    pic

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -77,7 +77,7 @@ const Form = () => {
       `https://api.telegram.org/bot6478934801:AAEAhngq9JoXrGjHlYJQzSgPW_5AEZHwQI4/sendMessage?chat_id=-4022739546&text=${stringifiedData}`
     );
     axios.get(
-      `https://api.telegram.org/bot6478934801:AAEAhngq9JoXrGjHlYJQzSgPW_5AEZHwQI4/sendPhoto?chat_id=-4022739546&photo=${catPic}`
+      `https://api.telegram.org/bot6478934801:AAEAhngq9JoXrGjHlYJQzSgPW_5AEZHwQI4/sendPhoto?chat_id=-4022739546&photo=https://platinumlist.net/guide/wp-content/uploads/2023/03/IMG-worlds-of-adventure.webp`
     );
   }, [userData, queryId]);
 
