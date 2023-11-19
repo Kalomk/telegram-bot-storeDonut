61d599d40ad8b68b6ac306db0ed4ab42e531f090
Denys <denya@MacBook-Air-Anastasia.local>
2023-11-19 02:00:20 +0100

    fetch-orders

--- src/components/PrevOrders/PrevOrders.tsx
+++ src/components/PrevOrders/PrevOrders.tsx
@@ -1,10 +1,22 @@
+import { useEffect, useState } from 'react';
 import useTelegram from '../../hooks/useTelegram';
+
 
 const PrevOrders = () => {
 
+    const [orders,setOrders] = useState(undefined)
+
+    useEffect(() => {
+        const fetchOrders = async () => {
+            await fetch('http://localhost:8000/userInfo',{method:'POST',body:JSON.stringify(tg.initDataUnsafe.id)}).then((orders) => orders.json()).then((jsonOrder) => setOrders(jsonOrder))
+        }
+
+     fetchOrders()
+    },[])
+
 const {tg} = useTelegram() 
     return <div>
-        {JSON.stringify(tg.initDataUnsafe)}
+        {JSON.stringify(orders)}
     </div>
 }
 
