60df6acb6bdf795eb78be133a5dceac6d97748fe
Denys <denya@MacBook-Air-Anastasia.local>
2023-11-25 16:15:36 +0100

    user-id

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -156,7 +156,6 @@ const Form = () => {
         <img style={{ width: 25, height: 25, marginBottom: 3 }} src={arrow} alt="" />
         <span>Кошик</span>
         <span>{chatId}</span>
-        <span>{JSON.stringify(user)}</span>
       </a>
       <div className="form">
         <h3>Введіть ваші данні </h3>
--- src/hooks/useTelegram.ts
+++ src/hooks/useTelegram.ts
@@ -19,7 +19,7 @@ const useTelegram = () => {
     onClose,
     user: tg.initDataUnsafe?.user,
     queryId: tg.initDataUnsafe?.query_id,
-    chatId: tg.initDataUnsafe?.id,
+    chatId: tg.initDataUnsafe?.user?.id,
     onToggleButton,
   };
 };
